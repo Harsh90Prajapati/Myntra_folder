35b7a8143f19a15f7d76b6e1af76225a116dcc2b
Harsh90Prajapati <harshprajapati904433@gmail.com>
2024-05-04 16:08:34 +0530

    changes done

--- Myntra_Clone/src/components/BottomBanner.jsx
+++ Myntra_Clone/src/components/BottomBanner.jsx
@@ -1,9 +1,9 @@
 const BottomBanner = () => {
   return (
     <div className="max-w-7xl mx-auto md:my-5">
-        <div className="flex lg:max-w-7xl sm:max-w-2xl md:max-w-4xl mx-auto  justify-between my-4 sm:my-10">
-            <img src="../img/banner/b16.jpg" alt="" className="w-60 sm:w-80 md:w-96  xl:w-auto" />
-            <img src="../img/banner/b18.jpg" alt="" className="w-60 sm:w-80 md:w-96 xl:w-auto" />
+        <div className="flex lg:max-w-7xl sm:max-w-2xl md:max-w-3xl max-w-md mx-auto md:justify-between justify-evenly my-4 sm:my-10">
+            <img src="../img/banner/b16.jpg" alt="" className=" w-40 sm:w-80 md:w-96 xl:w-auto" />
+            <img src="../img/banner/b18.jpg" alt="" className=" w-40 sm:w-80 md:w-96 xl:w-auto" />
         </div>
         <a href="">
            <img src="../img/banner/download.png" alt="" />
